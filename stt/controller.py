d73f9097d4aa7b059f68f285f2b04225cfba20f2
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-22 20:09:48 +0900

    fix: returnzero 동기화 처리 #9

--- stt/controller.py
+++ stt/controller.py
@@ -1,12 +1,8 @@
+
 from flask import Blueprint, request, jsonify
-import asyncio
-import stt.service as etri_service
 import stt.returnzero_service as returnzero_service
 
 stt = Blueprint('stt', __name__)
-
-async def call_service(service_function, file):
-    return await service_function(file)
 
 @stt.route('/', methods=['POST'])
 def speech_to_text():
@@ -16,9 +12,8 @@ def speech_to_text():
     if file.filename == '':
         return jsonify({'text': 'Error: No selected file'}), 400
     if file:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        status, response_data = loop.run_until_complete(call_service(returnzero_service.request_text, file))
-
-        return jsonify({'text': response_data}), status
+        try:
+            status, response_data = returnzero_service.request_text(file)
+            return jsonify({'text': response_data}), status
+        except Exception as e:
+            return jsonify({'text': str(e)}), 500
--- stt/returnzero_service.py
+++ stt/returnzero_service.py
@@ -6,10 +6,8 @@ load_dotenv()
 
 import json
 import requests
-import aiohttp
-import asyncio
-
-async def request_text(file):
+import time
+def request_text(file):
     client_id = os.getenv("RT_CLIENT_ID")
     client_secret = os.getenv("RT_CLIENT_SECRET")
 
@@ -35,43 +33,39 @@ async def request_text(file):
         "paragraph_splitter": {"max": 50}
     }
 
+    # 파일을 바이트 스트림으로 변환하여 추가
+    file.seek(0)  # 파일의 시작 부분으로 이동
+    files = {
+        'config': (None, json.dumps(config), 'application/json'),
+        'file': (file.filename, file, file.content_type)
+    }
+
     # 전사 요청 제출
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        data = aiohttp.FormData()
-        data.add_field('config', json.dumps(config), content_type='application/json')
-        
-        # 파일을 바이트 스트림으로 변환하여 추가
-        file.seek(0)  # 파일의 시작 부분으로 이동
-        data.add_field('file', file.read(), filename=file.filename, content_type=file.content_type)
+    transcribe_resp = requests.post(
+        'https://openapi.vito.ai/v1/transcribe',
+        headers={'Authorization': 'bearer ' + accessToken},
+        files=files
+    )
+    transcribe_resp.raise_for_status()
+    response_json = transcribe_resp.json()
+    transcription_id = response_json['id']
+    print(f"Transcription ID: {transcription_id}")
 
-        async with session.post(
-            'https://openapi.vito.ai/v1/transcribe',
-            headers={'Authorization': 'bearer ' + accessToken},
-            data=data
-        ) as resp:
-            resp.raise_for_status()
-            response_json = await resp.json()
-            transcription_id = response_json['id']
-            print(f"Transcription ID: {transcription_id}")
+    # 전사 상태 확인
+    status_url = f'https://openapi.vito.ai/v1/transcribe/{transcription_id}'
+    while True:
+        status_resp = requests.get(
+            status_url,
+            headers={'Authorization': 'bearer ' + accessToken}
+        )
+        print(f"Checking status at URL: {status_url}")
+        status_resp.raise_for_status()
+        status_data = status_resp.json()
+        print(f"Status Data: {status_data}")
 
-        # 전사 상태 확인
-        status_url = f'https://openapi.vito.ai/v1/transcribe/{transcription_id}'
-        print(f"Status URL: {status_url}")  # Status URL 확인용 출력
-        while True:
-            async with session.get(
-                status_url,
-                headers={'Authorization': 'bearer ' + accessToken}
-            ) as status_resp:
-                print(f"Checking status at URL: {status_url}")
-                if status_resp.status == 404:
-                    print(f"404 Error: {await status_resp.text()}")
-                status_resp.raise_for_status()
-                status_data = await status_resp.json()
-                print(f"Status Data: {status_data}")
-
-                if status_data['status'] == 'completed':
-                    return status_resp.status, status_data['results']
-                elif status_data['status'] == 'failed':
-                    return status_resp.status, "Transcription failed"
-                else:
-                    await asyncio.sleep(5)
+        if status_data['status'] == 'completed':
+            return status_resp.status_code, status_data['results']
+        elif status_data['status'] == 'failed':
+            return status_resp.status_code, "Transcription failed"
+        else:
+            time.sleep(5)
