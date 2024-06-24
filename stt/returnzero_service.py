21737b1ad3fddc184c7bdeb5f52299153db97752
t3rryahn <t3rryahn.dev@gmail.com>
2024-06-24 23:19:23 +0900

    refactor: 매 요청시마다 토큰발급을 받던것을 토큰 만료시간에만 재발급 받도록 개선

--- stt/returnzero_service.py
+++ stt/returnzero_service.py
@@ -1,12 +1,37 @@
 import os
 from dotenv import load_dotenv
+import json
+import requests
+import time
 
 # .env 파일 로드
 load_dotenv()
 
-import json
-import requests
-import time
+access_token = None
+token_expiry_time = None
+
+
+def get_access_token():
+    print("\n리턴제로 토큰 요청 시작")
+    global access_token, token_expiry_time
+    if access_token and token_expiry_time and time.time() < token_expiry_time:
+        return access_token
+
+    client_id = os.getenv("RT_CLIENT_ID")
+    client_secret = os.getenv("RT_CLIENT_SECRET")
+    auth_resp = requests.post(
+        'https://openapi.vito.ai/v1/authenticate',
+        data={
+            'client_id': client_id,
+            'client_secret': client_secret
+        }
+    )
+    auth_resp.raise_for_status()
+    access_token = auth_resp.json()['access_token']
+    token_expiry_time = time.time() + 3600 * 6  # assuming token is valid for 6 hour
+    return access_token
+
+
 def request_text(file):
     print("\n리턴제로 요청 시작")
     client_id = os.getenv("RT_CLIENT_ID")
