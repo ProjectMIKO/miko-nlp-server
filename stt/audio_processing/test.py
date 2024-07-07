f989a05dab5649b498cad323c171e4a01de43714
t3rryahn <t3rryahn.dev@gmail.com>
2024-07-08 05:26:57 +0900

    feat: 환경테스트 sox 추가

--- stt/audio_processing/test.py
+++ stt/audio_processing/test.py
@@ -1,6 +1,7 @@
 import torchaudio
 
 # torchaudio 백엔드를 설정하지 않아도 기본적으로 작동하는지 확인
+torchaudio.set_audio_backend("sox_io")
 print(torchaudio.get_audio_backend())
 
 # 샘플 wav 파일을 로드하여 ffmpeg가 제대로 작동하는지 확인합니다.
