a3e12cc9d2b4338316e60cd91e269454293f18ae
t3rryahn <t3rryahn.dev@gmail.com>
2024-07-08 05:22:58 +0900

    feat: 환경 테스트 코드 추가

---
+++ stt/audio_processing/test.py
@@ -0,0 +1,8 @@
+import torchaudio
+
+# torchaudio 백엔드를 설정하지 않아도 기본적으로 작동하는지 확인
+print(torchaudio.get_audio_backend())
+
+# 샘플 wav 파일을 로드하여 ffmpeg가 제대로 작동하는지 확인합니다.
+waveform, sample_rate = torchaudio.load("stt/audio_processing/aa.wav")
+print(waveform.shape, sample_rate)
