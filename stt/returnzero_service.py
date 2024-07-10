4ab5124631cdd4227d04d8fad878ca37b83e0433
t3rryahn <t3rryahn.dev@gmail.com>
2024-07-10 21:46:55 +0900

    refactor: 리턴제로 도메인 call 로 변경

--- stt/returnzero_service.py
+++ stt/returnzero_service.py
@@ -41,7 +41,7 @@ def request_text(file):
         access_token = get_access_token()
 
         config = {
-            'domain': 'GENERAL',
+            'domain': 'CALL',
             "use_diarization": False,
             "use_itn": True,
             "use_disfluency_filter": False,
