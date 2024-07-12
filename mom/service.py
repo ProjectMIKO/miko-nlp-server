1b2d8710d179b9ec746f6bc9e9d6278a129bc863
t3rryahn <t3rryahn.dev@gmail.com>
2024-07-12 13:05:39 +0900

    prompt 에 예제 정보 사용하지 않도록 추가

--- mom/service.py
+++ mom/service.py
@@ -19,7 +19,8 @@ async def process_message(messages):
     for chunk in chunks:
         prompt = f""" You are a meeting summarization bot. Your main task is to read the conversation, generate a 
         detailed meeting note body in html format in korean. do not write title. do not write conversation just 
-        write details Here is an example of a conversation and the desired output format:
+        write details Here is an example of a conversation and the desired output format. do not use the example 
+        information I provided in your output.:
     
         Example conversation:
         keyword(여행 계획): 여행 장소 및 관광지 회의 \n conversations: [speaker(준호): 우리 여행 가자. speaker(윤아): 
--- nlp_keyword/service.py
+++ nlp_keyword/service.py
@@ -13,8 +13,11 @@ async def process_message(user_message):
     준호: 그럼 점심 뭐 먹을까? 민수: 난 치킨이나 피자. 윤아: 난 국밥먹고싶어. 준호: 그럼 가까운 한우곰탕이나 먹으러가자. 민수: 그럼 그러자. 그럼 대전에서 어디 또 갈까?
     윤아: 식장산 야경이 유명하대. 식장산 가자."""
 
-    prompt = f"""
-    You are a meeting summarization bot. Your main task is to read the conversation, generate very short titles as keywords (nouns), and summarize the content into key points under the corresponding topics. There can be multiple main topics, and each main topic can have multiple subtopics (sub1 and sub2) with a vertex depth of up to 3 levels. Make sure to include sub2 for every sub1.
+    prompt = f""" You are a meeting summarization bot. Your main task is to read the conversation, generate very 
+    short titles as keywords (nouns), and summarize the content into key points under the corresponding topics. There 
+    can be multiple main topics, and each main topic can have multiple subtopics (sub1 and sub2) with a vertex depth 
+    of up to 3 levels. Make sure to include sub2 for every sub1. do not use the example information I provided in 
+    your output.
  
     
     Here is an example of a conversation and the desired output format:
