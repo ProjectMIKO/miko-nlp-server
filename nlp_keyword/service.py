d1302a455f2798fe0db6acc097354b982cc5569a
t3rryahn <t3rryahn.dev@gmail.com>
2024-07-19 13:06:38 +0900

    test: 키워드 응답 출력2

--- nlp_keyword/service.py
+++ nlp_keyword/service.py
@@ -145,7 +145,7 @@ async def process_message(user_message):
     )
 
     response_content = chat_completion.choices[0].message.content.strip()
-    print(f"GPT 응답: {response_content}")  # 응답 내용 출력
+    print(f"GPT 응답: {chat_completion}")  # 응답 내용 출력
     response_message = json.loads(chat_completion.choices[0].message.content.strip())
 
     idea = response_message.get("idea", [])
